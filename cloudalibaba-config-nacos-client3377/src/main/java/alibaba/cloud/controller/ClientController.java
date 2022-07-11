f5cc365afc47fe33a4365cdbdc39bd92b582a402
Kade <wangkai@youxin.cloud>
2022-07-11 19:42:59 +0800

    alibaba nacos study

---
+++ cloudalibaba-config-nacos-client3377/src/main/java/alibaba/cloud/controller/ClientController.java
@@ -0,0 +1,18 @@
+package alibaba.cloud.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RefreshScope
+public class ClientController {
+    @Value("${config.info}")  //在Nacos配置中心的配置文件内容
+    private String configInfo;
+
+    @GetMapping("/config/info")
+    public String getConfigInfo(){
+        return configInfo;
+    }
+}
